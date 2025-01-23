eabd4c8361083cccd21eb3c683337c104c0f9a3f
Naika <naikagovinda@gmail.com>
2025-01-23 15:45:52 +0700

    Добавлен метод main

---
+++ Praktikum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Praktikum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number;
+        number = input.nextInt();
+        System.out.println(number);
+    }
+}
