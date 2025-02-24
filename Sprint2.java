15838d792637432312154a70f42cf6c0be09138e
Naika <naikagovinda@gmail.com>
2025-02-24 13:22:07 +0700

    Добавил в Person больше параметров

---
+++ Sprint2.java
@@ -0,0 +1,34 @@
+public class Sprint2 {
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(person.print);
+    }
+    public static class Person {
+        public String print;
+        private Integer age;
+
+        public Person(String print) {
+            this.print = print;
+        }
+
+        public String print() {
+            return print;
+        }
+
+        public String getPrint() {
+            return print;
+        }
+
+        public void setPrint(String print) {
+            this.print = print;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+    }
+}
